408123381d31ba49238e9cceae61d31358c488bb
Nora Khalil <khalil.nor@northeastern.edu>
2025-04-17 11:54:38 -0400

    CO_CF_bond_dissociation_charge_separated: made initial groups.py, rules.py (untrained), and training folders with reactions. Edited CO_CF_bond_dissociation training folder to remove reactions involving charge separated resonance structures, and moved these reactions into training folder of CO_CF_bond_dissociation_charge_separated.

--- input/kinetics/families/CO_CF_bond_dissociation/training/reactions.py
+++ input/kinetics/families/CO_CF_bond_dissociation/training/reactions.py
@@ -20,18 +20,19 @@ Original entry: CF3OCFCF2 <=> CF2O+CF2CF2
 """,
 )
 
-entry(
-    index = 1,
-    label = "C3F6O-2 <=> CF2O + C2F4-2",
-    degeneracy = 3.0,
-    kinetics = Arrhenius(A=(2.43e+08,'s^-1'), n=1.22, Ea=(48702.3,'cal/mol'), T0=(1,'K')),
-    rank = 3,
-    longDesc = 
-"""
-Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation/
-Original entry: CF3OCCF3 <=> CF3CF+CF2O
-""",
-)
+# entry(
+#     index = 1,
+#     label = "C3F6O-2 <=> CF2O + C2F4-2",
+#     degeneracy = 3.0,
+#     kinetics = Arrhenius(A=(2.43e+08,'s^-1'), n=1.22, Ea=(48702.3,'cal/mol'), T0=(1,'K')),
+#     rank = 3,
+#     longDesc = 
+# """
+# Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation/
+# Original entry: CF3OCCF3 <=> CF3CF+CF2O
+# """,
+# )
+#moved to CO_CF_bond_dissociation_charge_separated/training
 
 entry(
     index = 2,
@@ -72,18 +73,19 @@ Original entry: C2F5OCFCF2 <=> CF3CFO+CF2CF2
 """,
 )
 
-entry(
-    index = 5,
-    label = "C4F8O-2 <=> C2F4O-2 + C2F4-2",
-    degeneracy = 2.0,
-    kinetics = Arrhenius(A=(1.53e+09,'s^-1'), n=0.9, Ea=(52188.2,'cal/mol'), T0=(1,'K')),
-    rank = 3,
-    longDesc = 
-"""
-Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation/
-Original entry: C2F5OCCF3 <=> CF3CFO+CF3CF
-""",
-)
+# entry(
+#     index = 5,
+#     label = "C4F8O-2 <=> C2F4O-2 + C2F4-2",
+#     degeneracy = 2.0,
+#     kinetics = Arrhenius(A=(1.53e+09,'s^-1'), n=0.9, Ea=(52188.2,'cal/mol'), T0=(1,'K')),
+#     rank = 3,
+#     longDesc = 
+# """
+# Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation/
+# Original entry: C2F5OCCF3 <=> CF3CFO+CF3CF
+# """,
+# )
+#moved to CO_CF_bond_dissociation_charge_separated/training
 
 entry(
     index = 6,
@@ -111,18 +113,19 @@ Original entry: C3F7OCFCF2 <=> C2F5CFO+CF2CF2
 """,
 )
 
-entry(
-    index = 8,
-    label = "C5F10O-2 <=> C3F6O-3 + C2F4-2",
-    degeneracy = 2.0,
-    kinetics = Arrhenius(A=(3.26e+08,'s^-1'), n=1.07, Ea=(51125.8,'cal/mol'), T0=(1,'K')),
-    rank = 3,
-    longDesc = 
-"""
-Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation/
-Original entry: C3F7OCCF3 <=> C2F5CFO+CF3CF
-""",
-)
+# entry(
+#     index = 8,
+#     label = "C5F10O-2 <=> C3F6O-3 + C2F4-2",
+#     degeneracy = 2.0,
+#     kinetics = Arrhenius(A=(3.26e+08,'s^-1'), n=1.07, Ea=(51125.8,'cal/mol'), T0=(1,'K')),
+#     rank = 3,
+#     longDesc = 
+# """
+# Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation/
+# Original entry: C3F7OCCF3 <=> C2F5CFO+CF3CF
+# """,
+# )
+#moved to CO_CF_bond_dissociation_charge_separated/training
 
 entry(
     index = 9,
@@ -176,31 +179,33 @@ Original entry: C2H5OCHCH2 <=> CH3CHO+CH2CH2
 """,
 )
 
-entry(
-    index = 13,
-    label = "C4H8O-2 <=> C2H4O + C2H4-2",
-    degeneracy = 2.0,
-    kinetics = Arrhenius(A=(4.11e+06,'s^-1'), n=1.75, Ea=(44653.7,'cal/mol'), T0=(1,'K')),
-    rank = 3,
-    longDesc = 
-"""
-Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation/
-Original entry: C2H5OCCH3 <=> CH3CHO+CH3CH
-""",
-)
+# entry(
+#     index = 13,
+#     label = "C4H8O-2 <=> C2H4O + C2H4-2",
+#     degeneracy = 2.0,
+#     kinetics = Arrhenius(A=(4.11e+06,'s^-1'), n=1.75, Ea=(44653.7,'cal/mol'), T0=(1,'K')),
+#     rank = 3,
+#     longDesc = 
+# """
+# Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation/
+# Original entry: C2H5OCCH3 <=> CH3CHO+CH3CH
+# """,
+# )
+#moved to CO_CF_bond_dissociation_charge_separated/training
 
-entry(
-    index = 14,
-    label = "C5H10O <=> C3H6O-2 + C2H4-2",
-    degeneracy = 2.0,
-    kinetics = Arrhenius(A=(1.12e+06,'s^-1'), n=1.87, Ea=(44732.4,'cal/mol'), T0=(1,'K')),
-    rank = 3,
-    longDesc = 
-"""
-Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation/
-Original entry: C3H7OCCH3 <=> C2H5CHO+CH3CH
-""",
-)
+# entry(
+#     index = 14,
+#     label = "C5H10O <=> C3H6O-2 + C2H4-2",
+#     degeneracy = 2.0,
+#     kinetics = Arrhenius(A=(1.12e+06,'s^-1'), n=1.87, Ea=(44732.4,'cal/mol'), T0=(1,'K')),
+#     rank = 3,
+#     longDesc = 
+# """
+# Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation/
+# Original entry: C3H7OCCH3 <=> C2H5CHO+CH3CH
+# """,
+# )
+#moved to CO_CF_bond_dissociation_charge_separated/training
 
 entry(
     index = 15,
---
+++ input/kinetics/families/CO_CF_bond_dissociation_charge_separated/training/reactions.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "CO_CF_bond_dissociation_charge_separated/training"
+shortDesc = "Reaction kinetics used to generate rate rules"
+longDesc = """
+Put kinetic parameters for specific reactions in this file to use as a
+training set for generating rate rules to populate this kinetics family.
+"""
+
+entry(
+    index = 1,
+    label = "C3F6O-2 <=> CF2O + C2F4-2",
+    degeneracy = 3.0,
+    kinetics = Arrhenius(A=(2.43e+08,'s^-1'), n=1.22, Ea=(48702.3,'cal/mol'), T0=(1,'K')),
+    rank = 3,
+    longDesc = 
+"""
+Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation_charge_separated/
+Original entry: CF3OCCF3 <=> CF3CF+CF2O
+""",
+)
+
+entry(
+    index = 5,
+    label = "C4F8O-2 <=> C2F4O-2 + C2F4-2",
+    degeneracy = 2.0,
+    kinetics = Arrhenius(A=(1.53e+09,'s^-1'), n=0.9, Ea=(52188.2,'cal/mol'), T0=(1,'K')),
+    rank = 3,
+    longDesc = 
+"""
+Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation_charge_separated/
+Original entry: C2F5OCCF3 <=> CF3CFO+CF3CF
+""",
+)
+
+entry(
+    index = 8,
+    label = "C5F10O-2 <=> C3F6O-3 + C2F4-2",
+    degeneracy = 2.0,
+    kinetics = Arrhenius(A=(3.26e+08,'s^-1'), n=1.07, Ea=(51125.8,'cal/mol'), T0=(1,'K')),
+    rank = 3,
+    longDesc = 
+"""
+Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation_charge_separated/
+Original entry: C3F7OCCF3 <=> C2F5CFO+CF3CF
+""",
+)
+
+entry(
+    index = 13,
+    label = "C4H8O-2 <=> C2H4O + C2H4-2",
+    degeneracy = 2.0,
+    kinetics = Arrhenius(A=(4.11e+06,'s^-1'), n=1.75, Ea=(44653.7,'cal/mol'), T0=(1,'K')),
+    rank = 3,
+    longDesc = 
+"""
+Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation_charge_separated/
+Original entry: C2H5OCCH3 <=> CH3CHO+CH3CH
+""",
+)
+
+entry(
+    index = 14,
+    label = "C5H10O <=> C3H6O-2 + C2H4-2",
+    degeneracy = 2.0,
+    kinetics = Arrhenius(A=(1.12e+06,'s^-1'), n=1.87, Ea=(44732.4,'cal/mol'), T0=(1,'K')),
+    rank = 3,
+    longDesc = 
+"""
+Training reaction from kinetics library: PFAS_HPL_Test/CO_CF_bond_dissociation_charge_separated/
+Original entry: C3H7OCCH3 <=> C2H5CHO+CH3CH
+""",
+)
